4ef2cff614e46c84bedfcfcfc73c02ab5a770cea
Graham Hoyes <accounts@grahamhoyes.com>
2020-10-29 18:59:09 -0400

    Require login for delete

--- project/app.py
+++ project/app.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from functools import wraps
 
 from flask import (
     Flask,
@@ -35,6 +36,14 @@ db = SQLAlchemy(app)
 
 from project import models
 
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not session.get('logged_in'):
+            flash('Please log in.')
+            return jsonify({'status': 0, 'message': 'Please log in.'}), 401
+        return f(*args, **kwargs)
+    return decorated_function
 
 @app.route("/")
 def index():
@@ -80,6 +89,7 @@ def logout():
 
 
 @app.route("/delete/<int:post_id>", methods=["GET"])
+@login_required
 def delete_entry(post_id):
     """Deletes post from database."""
     try:
